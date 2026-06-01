f707bab18e72db63e89d4aa3b216471edc8e6699
Usagi Network <the@usagi.network>
2026-06-02 07:22:01 +0900

    Split MiniJson and avoid unnecessary escaping allocations

---
+++ unity/un-avatar-unity-exporter/Editor/MiniJson.Serialize.cs
@@ -0,0 +1,111 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Security.Cryptography;
+using System.Text;
+using UnityEditor;
+using UnityEngine;
+
+namespace UNAvatar.UnityExporter
+{
+    internal static partial class MiniJson
+    {
+        private static void WriteValue(StringBuilder sb, object value)
+        {
+            if (value == null)
+            {
+                sb.Append("null");
+                return;
+            }
+
+            switch (value)
+            {
+                case string s:
+                    sb.Append('"').Append(EscapeString(s)).Append('"');
+                    break;
+                case bool b:
+                    sb.Append(b ? "true" : "false");
+                    break;
+                case byte _:
+                case sbyte _:
+                case short _:
+                case ushort _:
+                case int _:
+                case uint _:
+                case long _:
+                case ulong _:
+                case float _:
+                case double _:
+                case decimal _:
+                    sb.Append(Convert.ToString(value, CultureInfo.InvariantCulture));
+                    break;
+                case IDictionary<string, object> map:
+                    WriteObject(sb, map);
+                    break;
+                case IDictionary dictionary:
+                    WriteDictionary(sb, dictionary);
+                    break;
+                case IEnumerable enumerable:
+                    WriteArray(sb, enumerable);
+                    break;
+                default:
+                    sb.Append('"').Append(EscapeString(Convert.ToString(value, CultureInfo.InvariantCulture))).Append('"');
+                    break;
+            }
+        }
+
+        private static void WriteObject(StringBuilder sb, IDictionary<string, object> map)
+        {
+            sb.Append('{');
+            var first = true;
+            foreach (var item in map)
+            {
+                if (!first)
+                {
+                    sb.Append(',');
+                }
+                first = false;
+                sb.Append('"').Append(EscapeString(item.Key)).Append("\":");
+                WriteValue(sb, item.Value);
+            }
+            sb.Append('}');
+        }
+
+        private static void WriteDictionary(StringBuilder sb, IDictionary map)
+        {
+            sb.Append('{');
+            var first = true;
+            foreach (DictionaryEntry item in map)
+            {
+                if (!first)
+                {
+                    sb.Append(',');
+                }
+                first = false;
+                sb.Append('"').Append(EscapeString(Convert.ToString(item.Key, CultureInfo.InvariantCulture))).Append("\":");
+                WriteValue(sb, item.Value);
+            }
+            sb.Append('}');
+        }
+
+        private static void WriteArray(StringBuilder sb, IEnumerable values)
+        {
+            sb.Append('[');
+            var first = true;
+            foreach (var item in values)
+            {
+                if (!first)
+                {
+                    sb.Append(',');
+                }
+                first = false;
+                WriteValue(sb, item);
+            }
+            sb.Append(']');
+        }
+    }
+}
--- unity/un-avatar-unity-exporter/Editor/MiniJson.cs
+++ unity/un-avatar-unity-exporter/Editor/MiniJson.cs
@@ -12,7 +12,7 @@ using UnityEngine;
 
 namespace UNAvatar.UnityExporter
 {
-    internal static class MiniJson
+    internal static partial class MiniJson
     {
         public static object Deserialize(string json)
         {
@@ -28,9 +28,33 @@ namespace UNAvatar.UnityExporter
 
         public static string EscapeString(string value)
         {
-            var sb = new StringBuilder();
-            foreach (var c in value)
+            if (string.IsNullOrEmpty(value))
             {
+                return value ?? "";
+            }
+            var firstEscaped = -1;
+            for (var i = 0; i < value.Length; i++)
+            {
+                var c = value[i];
+                if (c == '"' || c == '\\' || c < 0x20)
+                {
+                    firstEscaped = i;
+                    break;
+                }
+            }
+            if (firstEscaped < 0)
+            {
+                return value;
+            }
+
+            var sb = new StringBuilder();
+            if (firstEscaped > 0)
+            {
+                sb.Append(value, 0, firstEscaped);
+            }
+            for (var i = firstEscaped; i < value.Length; i++)
+            {
+                var c = value[i];
                 switch (c)
                 {
                     case '"': sb.Append("\\\""); break;
@@ -55,302 +79,5 @@ namespace UNAvatar.UnityExporter
             }
             return sb.ToString();
         }
-
-        private static void WriteValue(StringBuilder sb, object value)
-        {
-            if (value == null)
-            {
-                sb.Append("null");
-                return;
-            }
-
-            switch (value)
-            {
-                case string s:
-                    sb.Append('"').Append(EscapeString(s)).Append('"');
-                    break;
-                case bool b:
-                    sb.Append(b ? "true" : "false");
-                    break;
-                case byte _:
-                case sbyte _:
-                case short _:
-                case ushort _:
-                case int _:
-                case uint _:
-                case long _:
-                case ulong _:
-                case float _:
-                case double _:
-                case decimal _:
-                    sb.Append(Convert.ToString(value, CultureInfo.InvariantCulture));
-                    break;
-                case IDictionary<string, object> map:
-                    WriteObject(sb, map);
-                    break;
-                case IDictionary dictionary:
-                    WriteDictionary(sb, dictionary);
-                    break;
-                case IEnumerable enumerable:
-                    WriteArray(sb, enumerable);
-                    break;
-                default:
-                    sb.Append('"').Append(EscapeString(Convert.ToString(value, CultureInfo.InvariantCulture))).Append('"');
-                    break;
-            }
-        }
-
-        private static void WriteObject(StringBuilder sb, IDictionary<string, object> map)
-        {
-            sb.Append('{');
-            var first = true;
-            foreach (var item in map)
-            {
-                if (!first)
-                {
-                    sb.Append(',');
-                }
-                first = false;
-                sb.Append('"').Append(EscapeString(item.Key)).Append("\":");
-                WriteValue(sb, item.Value);
-            }
-            sb.Append('}');
-        }
-
-        private static void WriteDictionary(StringBuilder sb, IDictionary map)
-        {
-            sb.Append('{');
-            var first = true;
-            foreach (DictionaryEntry item in map)
-            {
-                if (!first)
-                {
-                    sb.Append(',');
-                }
-                first = false;
-                sb.Append('"').Append(EscapeString(Convert.ToString(item.Key, CultureInfo.InvariantCulture))).Append("\":");
-                WriteValue(sb, item.Value);
-            }
-            sb.Append('}');
-        }
-
-        private static void WriteArray(StringBuilder sb, IEnumerable values)
-        {
-            sb.Append('[');
-            var first = true;
-            foreach (var item in values)
-            {
-                if (!first)
-                {
-                    sb.Append(',');
-                }
-                first = false;
-                WriteValue(sb, item);
-            }
-            sb.Append(']');
-        }
-
-        private sealed class Parser
-        {
-            private readonly string json;
-            private int index;
-
-            public Parser(string json)
-            {
-                this.json = json ?? "";
-            }
-
-            public object Parse()
-            {
-                var value = ParseValue();
-                SkipWhitespace();
-                return value;
-            }
-
-            private object ParseValue()
-            {
-                SkipWhitespace();
-                if (index >= json.Length)
-                {
-                    throw new InvalidDataException("Unexpected end of JSON.");
-                }
-
-                switch (json[index])
-                {
-                    case '{': return ParseObject();
-                    case '[': return ParseArray();
-                    case '"': return ParseString();
-                    case 't': Expect("true"); return true;
-                    case 'f': Expect("false"); return false;
-                    case 'n': Expect("null"); return null;
-                    default: return ParseNumber();
-                }
-            }
-
-            private Dictionary<string, object> ParseObject()
-            {
-                Expect('{');
-                var result = new Dictionary<string, object>();
-                SkipWhitespace();
-                if (TryConsume('}'))
-                {
-                    return result;
-                }
-                while (true)
-                {
-                    var key = ParseString();
-                    SkipWhitespace();
-                    Expect(':');
-                    result[key] = ParseValue();
-                    SkipWhitespace();
-                    if (TryConsume('}'))
-                    {
-                        return result;
-                    }
-                    Expect(',');
-                }
-            }
-
-            private List<object> ParseArray()
-            {
-                Expect('[');
-                var result = new List<object>();
-                SkipWhitespace();
-                if (TryConsume(']'))
-                {
-                    return result;
-                }
-                while (true)
-                {
-                    result.Add(ParseValue());
-                    SkipWhitespace();
-                    if (TryConsume(']'))
-                    {
-                        return result;
-                    }
-                    Expect(',');
-                }
-            }
-
-            private string ParseString()
-            {
-                Expect('"');
-                var sb = new StringBuilder();
-                while (index < json.Length)
-                {
-                    var c = json[index++];
-                    if (c == '"')
-                    {
-                        return sb.ToString();
-                    }
-                    if (c != '\\')
-                    {
-                        sb.Append(c);
-                        continue;
-                    }
-                    if (index >= json.Length)
-                    {
-                        throw new InvalidDataException("Invalid JSON string escape.");
-                    }
-                    var escaped = json[index++];
-                    switch (escaped)
-                    {
-                        case '"': sb.Append('"'); break;
-                        case '\\': sb.Append('\\'); break;
-                        case '/': sb.Append('/'); break;
-                        case 'b': sb.Append('\b'); break;
-                        case 'f': sb.Append('\f'); break;
-                        case 'n': sb.Append('\n'); break;
-                        case 'r': sb.Append('\r'); break;
-                        case 't': sb.Append('\t'); break;
-                        case 'u':
-                            if (index + 4 > json.Length)
-                            {
-                                throw new InvalidDataException("Invalid JSON unicode escape.");
-                            }
-                            var hex = json.Substring(index, 4);
-                            sb.Append((char)int.Parse(hex, NumberStyles.HexNumber, CultureInfo.InvariantCulture));
-                            index += 4;
-                            break;
-                        default:
-                            throw new InvalidDataException("Invalid JSON string escape.");
-                    }
-                }
-                throw new InvalidDataException("Unterminated JSON string.");
-            }
-
-            private double ParseNumber()
-            {
-                var start = index;
-                if (json[index] == '-')
-                {
-                    index++;
-                }
-                while (index < json.Length && char.IsDigit(json[index]))
-                {
-                    index++;
-                }
-                if (index < json.Length && json[index] == '.')
-                {
-                    index++;
-                    while (index < json.Length && char.IsDigit(json[index]))
-                    {
-                        index++;
-                    }
-                }
-                if (index < json.Length && (json[index] == 'e' || json[index] == 'E'))
-                {
-                    index++;
-                    if (index < json.Length && (json[index] == '+' || json[index] == '-'))
-                    {
-                        index++;
-                    }
-                    while (index < json.Length && char.IsDigit(json[index]))
-                    {
-                        index++;
-                    }
-                }
-                return double.Parse(json.Substring(start, index - start), CultureInfo.InvariantCulture);
-            }
-
-            private void SkipWhitespace()
-            {
-                while (index < json.Length && char.IsWhiteSpace(json[index]))
-                {
-                    index++;
-                }
-            }
-
-            private bool TryConsume(char expected)
-            {
-                SkipWhitespace();
-                if (index < json.Length && json[index] == expected)
-                {
-                    index++;
-                    return true;
-                }
-                return false;
-            }
-
-            private void Expect(char expected)
-            {
-                SkipWhitespace();
-                if (index >= json.Length || json[index] != expected)
-                {
-                    throw new InvalidDataException("Expected `" + expected + "` in JSON.");
-                }
-                index++;
-            }
-
-            private void Expect(string expected)
-            {
-                if (index + expected.Length > json.Length || json.Substring(index, expected.Length) != expected)
-                {
-                    throw new InvalidDataException("Expected `" + expected + "` in JSON.");
-                }
-                index += expected.Length;
-            }
-        }
     }
 }
-
