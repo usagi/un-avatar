63db7e8d9a8e6961aa91959a6e28ce2fa85e7a03
Usagi Network <the@usagi.network>
2026-06-28 16:44:00 +0900

    fix #15 group texture quality controls

--- apps/un-avatar-supervisor/src/lib/ProfileQualityTextureFields.svelte
+++ apps/un-avatar-supervisor/src/lib/ProfileQualityTextureFields.svelte
@@ -3,7 +3,7 @@
 	import ProfileSelectField from "./ProfileSelectField.svelte";
 	import ProfileToggleField from "./ProfileToggleField.svelte";
 	import type { ProfileSettingValue, QualitySetting } from "./profileTypes";
-	import { BCN_CPU_THREAD_SELECT_OPTIONS, TEXTURE_SELECT_FIELDS } from "./qualityOptions";
+	import { BCN_CPU_THREAD_SELECT_OPTIONS, TEXTURE_BASE_SELECT_FIELDS, TEXTURE_COMPRESSION_SELECT_FIELDS } from "./qualityOptions";
 
 	export let setting: Pick<
 		QualitySetting,
@@ -23,31 +23,49 @@
 	}
 </script>
 
-{#each TEXTURE_SELECT_FIELDS as field}
+<fieldset class="quality-fieldset">
+	<legend>{$_("profiles.editor.texture_sampling")}</legend>
+	{#each TEXTURE_BASE_SELECT_FIELDS as field}
+		<ProfileSelectField
+			label={$_(field.labelKey)}
+			hint={$_(field.hintKey)}
+			value={setting[field.key]}
+			disabled={busy}
+			options={field.options}
+			onChange={(value) => onUpdateSettingValue(field.field, value)}
+		/>
+	{/each}
+</fieldset>
+
+<fieldset class="quality-fieldset">
+	<legend>{$_("profiles.editor.texture_compression_cache")}</legend>
+	{#each TEXTURE_COMPRESSION_SELECT_FIELDS as field}
+		<ProfileSelectField
+			label={$_(field.labelKey)}
+			hint={$_(field.hintKey)}
+			value={setting[field.key]}
+			disabled={busy}
+			options={field.options}
+			onChange={(value) => onUpdateSettingValue(field.field, value)}
+		/>
+	{/each}
 	<ProfileSelectField
-		label={$_(field.labelKey)}
-		hint={$_(field.hintKey)}
-		value={setting[field.key]}
+		label={$_("profiles.editor.bcn_cpu_threads")}
+		hint={$_("profiles.hints.quality.bcn_cpu_threads")}
+		value={String(setting.block_compression_cpu_threads)}
 		disabled={busy}
-		options={field.options}
-		onChange={(value) => onUpdateSettingValue(field.field, value)}
+		options={BCN_CPU_THREAD_SELECT_OPTIONS}
+		onChange={updateCpuThreadCount}
 	/>
-{/each}
-<ProfileSelectField
-	label={$_("profiles.editor.bcn_cpu_threads")}
-	hint={$_("profiles.hints.quality.bcn_cpu_threads")}
-	value={String(setting.block_compression_cpu_threads)}
-	disabled={busy}
-	options={BCN_CPU_THREAD_SELECT_OPTIONS}
-	onChange={updateCpuThreadCount}
-/>
-<ProfileToggleField
-	label={$_("profiles.editor.processed_cache")}
-	hint={$_("profiles.editor.processed_cache_hint")}
-	checked={setting.processed_texture_cache}
-	disabled={busy}
-	onChange={(checked) => onUpdateSettingValue("render_quality.processed_texture_cache", checked)}
-/>
+	<ProfileToggleField
+		label={$_("profiles.editor.processed_cache")}
+		hint={$_("profiles.editor.processed_cache_hint")}
+		checked={setting.processed_texture_cache}
+		disabled={busy}
+		onChange={(checked) => onUpdateSettingValue("render_quality.processed_texture_cache", checked)}
+	/>
+</fieldset>
+
 <ProfileToggleField
 	label={$_("profiles.editor.skin_tone_matching")}
 	hint={$_("profiles.editor.skin_tone_matching_hint")}
